344816f73caaed7e0cf5ab357c4f9ab7a0c85f66
Nicholas Oshukany <ozzie347@hotmail.com>
2017-01-23 12:31:19 -0500

    first commit

---
+++ COMP1004-W2017-Lesson3/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_W2017_Lesson3
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculatorButton_Click(object sender, EventArgs e)
+        {
+            Button CalculatorButton = sender as Button;
+
+            ResultTextBox.Text += CalculatorButton.Text;
+        }
+    }
+}
---
+++ COMP1004-W2017-Lesson3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace COMP1004_W2017_Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Calculator());
+        }
+    }
+}
